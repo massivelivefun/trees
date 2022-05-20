f6d9545b87661286a551513480271a7df474b5e9
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-05-19 19:09:21 -0700

    Prevent mutations on queue pops

--- src/trees/tree_operations.py
+++ src/trees/tree_operations.py
@@ -1,6 +1,7 @@
 from collections import deque
 from .node import Node
 
+
 class TreeOperations:
     @staticmethod
     def bfs(nd: Node) -> list:
@@ -10,7 +11,7 @@ class TreeOperations:
             queue.append(nd)
             # while queue has element returns true
             while queue:
-                v = queue.popleft()
+                v = queue.popleft()  # pragma: no mutate
                 if v:
                     new_list.append(v.contents)
                     print(v.contents)
@@ -22,7 +23,6 @@ class TreeOperations:
                         queue.append(v.right)
         return new_list
 
-
     @staticmethod
     def pre_order(nd: Node) -> list:
         new_list = []
@@ -32,14 +32,13 @@ class TreeOperations:
             new_list += TreeOperations.pre_order(nd.right)
         return new_list
 
-
     @staticmethod
     def max_depth(nd: Node) -> int:
         def max_depth_func(nd: Node, level: int) -> int:
             # if nd is not None
             if nd:
                 return max(max_depth_func(nd.left, level + 1),
-                    max_depth_func(nd.right, level + 1))
+                           max_depth_func(nd.right, level + 1))
             # else return the level
             else:
                 return level
